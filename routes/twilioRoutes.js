df23cd9402970e90a33a9a62214b96957548ed27
Matthew Elliott <mse951993@gmail.com>
2019-04-27 13:13:11 -0700

    Basic texting functions working

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -25,11 +25,80 @@ module.exports = app => {
 
 
     app.post("/api/twilio/sms", (req, res) => {
+        console.log("Incoming message body");
+        console.log(req.body);
         //Check to see if an unfinished text exists with that incoming number.
+        db.Text
+            .find({ customerPhonenumber: req.body.From, reviewComplete: false })
+            .then(dbText => {
+                console.log("Response from initial database query");
+                console.log(dbText);
+                //if it does, find what step we are at
+                if (dbText.length > 0) {
+                    //check if the message is just a number. If it is, respond back with a message prompting them for a review.
+                    //could implement other checking on the message to ensure it's a valid review. I'm not sure how to do that yet, though.
+                    if (isNaN(req.body.Body)) {
+                        //If the review complete is false and a record already exists, add the body of what they are sending to the userComment
+                        //part of the Text. Push their text into the messages array to keep track of what they are sending. Mark review as complete.
+                        db.Text
+                            .findOneAndUpdate({ _id: dbText[0]._id }, { userComment: req.body.Body, $push: { messages: { textBody: req.body.Body } }, reviewComplete: true, reviewValid: true })
+                            .then(updatedDbText => {
+                                const twiml = new MessagingResponse();
+                                twiml.message("Thank You for your additional comments!");
+                                res.writeHead(200, { 'Content-Type': 'text/xml' });
+                                res.end(twiml.toString());
+                            })
+                            .catch(err => console.log(err));
+                    }
+                    //handling if they just sent back a number
+                    else {
+                        db.Text
+                            .findOneAndUpdate({ _id: dbText[0]._id }, {$push: { messages: { textBody: req.body.Body } } })
+                            .then(updatedDbText => {
+                                const twiml = new MessagingResponse();
+                                twiml.message("Please respond with any additional comments you would like to add. Your last message didn't look like a comment...");
+                                res.writeHead(200, { 'Content-Type': 'text/xml' });
+                                res.end(twiml.toString());
+                            })
+                            .catch(err => console.log(err));
+                    }
+                }
+                //if it doesn't then create a new text. Search for the locations matching the number the text was sent to
+                else {
+                    //Find the associated location by the phonenumber the consumer texted
+                    db.Location
+                        .findOne({ phonenumber: req.body.To })
+                        .then(dbLocation => {
+                            //create new text in the database
+                            db.Text.create({
+                                customerPhonenumber: req.body.From,
+                                locationPhonenumber: req.body.To,
+                                messages: [
+                                    { textBody: req.body.Body }
+                                ],
+                                reviewValid: true,
+                                rating: parseInt(req.body.Body.trim()),
+                                client_id: dbLocation.userId
+                            })
+                                .then(newDbText => {
+                                    const twiml = new MessagingResponse();
+                                    twiml.message("Thank You for your review! If you would like to leave an additional comment, respond to this message.");
+                                    res.writeHead(200, { 'Content-Type': 'text/xml' });
+                                    res.end(twiml.toString());
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                    const twiml = new MessagingResponse();
+                                    twiml.message("Please send a valid rating. Numbers 1 - 10 are accepted.");
+                                    res.writeHead(200, { 'Content-Type': 'text/xml' });
+                                    res.end(twiml.toString());
+                                });
 
-            //if it doesn't then create a new text. Search for the locations matching the number the text was sent to
+                        })
+                        .catch(err => console.log(err));
+                }
+            })
 
-            //if it does, find what step we are at
     });
 
 
