2ffd7cd0a9748c1b1ad69577b91d280f48db803e
Matthew Elliott <mse951993@gmail.com>
2019-05-02 13:34:07 -0700

    fixed some stylings

--- client/src/pages/TwilioResponses/index.js
+++ client/src/pages/TwilioResponses/index.js
@@ -46,65 +46,54 @@ class TwilioResponses extends Component {
     render() {
         const styles = {
             responseInput: {
-                width: "50%",
-            },
-            responseLabel: {
-            },
-            responseButton: {
+                width: "100%",
+                marginBottom: "20px"
             }
         }
         return (
-          <div className="wrapper">
-      <Sidebar {...this.props} routes={dashboardRoutes} />
-      <div className="main-panel" ref="mainPanel">
-        <div style={{ marginBottom: "50px" }}><Header dashColor={"black"} {...this.props} /></div>
-        <hr />
-        <div className="container">
-                <h3>Text Responses</h3>
-                <Popup
-                    trigger={<label style={styles.responseLabel} htmlFor="surResValid">Valid Survey Response: </label>}
-                    position="right top"
-                    on="hover">
-                    <p>Define the response to send to a customer upon receipt of a valid 1-10 review.</p>
-                    <p>Default message: "Thank You for the feedback! If you would like to add additional comments, just respond to this number!"</p>
-                </Popup>
-                <br></br>
-                <textarea value={this.state.surResValid} onChange={this.handleInputChange} style={styles.responseInput} type="text" name="surResValid"></textarea>
-                <br></br>
-                <Popup
-                    trigger={<label style={styles.responseLabel} htmlFor="surResInvalid">Invalid Survey Response: </label>}
-                    position="right top"
-                    on="hover">
-                    <p>Define the response to send to a customer upon receipt of a invalid 1-10 review.</p>
-                    <p>Default message: "Please respond with a rating of 1-10!"</p>
-                </Popup>
-                <br></br>
-                <textarea value={this.state.surResInvalid} onChange={this.handleInputChange} style={styles.responseInput} type="text" name="surResInvalid"></textarea>
-                <br></br>
-                <Popup
-                    trigger={<label style={styles.responseLabel} htmlFor="comResValid">Valid Comment Response: </label>}
-                    position="right top"
-                    on="hover">
-                    <p>Define the response to send to a customer upon receipt of valid additional comments.</p>
-                    <p>Default message: "Thank You for the feedback! Your comments are appreciated."</p>
-                </Popup>
-                <br></br>
-                <textarea value={this.state.comResValid} onChange={this.handleInputChange} style={styles.responseInput} type="text" name="comResValid"></textarea>
-                <br></br>
-                <Popup
-                    trigger={<label style={styles.responseLabel} htmlFor="comResInvalid">Invalid Comment Response: </label>}
-                    position="right top"
-                    on="hover">
-                    <p>Define the response to send to a customer upon receipt of invalid additional comments.</p>
-                    <p>Default message: "Please respond with any additional comments you have. That last message didn't look like a comment..."</p>
-                </Popup>
-                <br></br>
-                <textarea value={this.state.comResInvalid} onChange={this.handleInputChange} style={styles.responseInput} type="text" name="comResInvalid"></textarea>
-                <br></br>
-                <button style={styles.responseButton} onClick={this.handleTwilioResponsesSubmit}>Submit Responses</button>
+            <div className="wrapper">
+                <Sidebar {...this.props} routes={dashboardRoutes} />
+                <div className="main-panel" ref="mainPanel">
+                    <div style={{ marginBottom: "50px" }}><Header dashColor={"black"} {...this.props} /></div>
+                    <hr />
+                    <div className="container">
+                        <h3>Text Responses</h3>
+                        <Popup
+                            trigger={<label style={styles.responseLabel} htmlFor="surResValid">Valid Survey Response: </label>}
+                            position="right top"
+                            on="hover">
+                            <p>Define the response to send to a customer upon receipt of a valid 1-10 review.</p>
+                            <p>Default message: "Thank You for the feedback! If you would like to add additional comments, just respond to this number!"</p>
+                        </Popup>
+                        <input className="form-control" value={this.state.surResValid} onChange={this.handleInputChange} style={styles.responseInput} type="text" name="surResValid"></input>
+                        <Popup
+                            trigger={<label style={styles.responseLabel} htmlFor="surResInvalid">Invalid Survey Response: </label>}
+                            position="right top"
+                            on="hover">
+                            <p>Define the response to send to a customer upon receipt of a invalid 1-10 review.</p>
+                            <p>Default message: "Please respond with a rating of 1-10!"</p>
+                        </Popup>
+                        <input className="form-control" value={this.state.surResInvalid} onChange={this.handleInputChange} style={styles.responseInput} type="text" name="surResInvalid"></input>
+                        <Popup
+                            trigger={<label style={styles.responseLabel} htmlFor="comResValid">Valid Comment Response: </label>}
+                            position="right top"
+                            on="hover">
+                            <p>Define the response to send to a customer upon receipt of valid additional comments.</p>
+                            <p>Default message: "Thank You for the feedback! Your comments are appreciated."</p>
+                        </Popup>
+                        <input className="form-control" value={this.state.comResValid} onChange={this.handleInputChange} style={styles.responseInput} type="text" name="comResValid"></input>
+                        <Popup
+                            trigger={<label style={styles.responseLabel} htmlFor="comResInvalid">Invalid Comment Response: </label>}
+                            position="right top"
+                            on="hover">
+                            <p>Define the response to send to a customer upon receipt of invalid additional comments.</p>
+                            <p>Default message: "Please respond with any additional comments you have. That last message didn't look like a comment..."</p>
+                        </Popup>
+                        <input className="form-control" value={this.state.comResInvalid} onChange={this.handleInputChange} style={styles.responseInput} type="text" name="comResInvalid"></input>
+                        <button className="btn btn-primary" style={styles.responseButton} onClick={this.handleTwilioResponsesSubmit}>Submit Responses</button>
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
         )
     }
 
