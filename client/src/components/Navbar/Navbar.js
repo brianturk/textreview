46de5e0b9260942953911da245e5bbc085da914c
Brian Turk <bturksub@gmail.com>
2019-05-06 11:26:02 -0700

    made logo narrower because it wasn't the right dimension on the signup and login page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -44,7 +44,7 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container">
-                    <Link className="navbar-brand" to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
+                    <Link className="navbar-brand" to="/"><img src={logo} className="App-logo" alt="logo" style={{width: '60px'}}/></Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
